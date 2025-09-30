ab2884465480924ee0b0049042996c696d08e732
Dhruv <benegaldhruv2312@gmail.com>
2025-09-30 23:04:05 +0800

    Misconfig reversed

--- infra/modules/storage/main.tf
+++ infra/modules/storage/main.tf
@@ -31,8 +31,8 @@ resource "aws_s3_bucket_versioning" "test" {
 # Block public access to the bucket
 resource "aws_s3_bucket_public_access_block" "test" {
     bucket = aws_s3_bucket.test.id
-    block_public_acls       = false
-    block_public_policy     = false
-    ignore_public_acls      = false
-    restrict_public_buckets = false
+    block_public_acls       = true
+    block_public_policy     = true
+    ignore_public_acls      = true
+    restrict_public_buckets = true
 }
